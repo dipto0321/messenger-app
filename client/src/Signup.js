cd5273aadd3436d35b6e12104b12a29dc442e472
Dipto Karmakar <diptokmk47@gmail.com>
2021-06-07 23:20:30 +0600

    :lipstick: Design login page left side

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
+import { withStyles, useTheme } from "@material-ui/core/styles";
 import {
   Grid,
   Box,
@@ -12,9 +13,82 @@ import {
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
 
+import backgroundImg from "./assets/bg-img.png";
+import bubbleImg from "./assets/bubble.svg";
+
+const styles = (theme) => ({
+  root: {
+    height: "100vh",
+  },
+  boxContainer: {
+    backgroundImage: `linear-gradient(rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)),url(${backgroundImg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "100vw 100vh",
+    width: "100vw",
+    height: "100vh",
+    [theme.breakpoints.up("sm")]: {
+      backgroundSize: "100vw",
+    },
+    [theme.breakpoints.up("md")]: {
+      backgroundSize: "50vw",
+      width: "50vw",
+    },
+    [theme.breakpoints.up("lg")]: {
+      backgroundSize: "40vw",
+      width: "40vw",
+    },
+  },
+  boxImage: {
+    width: "16vw",
+    position: "relative",
+    top: -80,
+    [theme.breakpoints.up("sm")]: {
+      top: 95,
+      width: "15vw",
+    },
+    [theme.breakpoints.up("md")]: {
+      top: -55,
+      width: "8vw",
+    },
+    [theme.breakpoints.up("lg")]: {
+      top: 0,
+      width: "6vw",
+    },
+  },
+  boxText: {
+    fontSize: "1.5rem",
+    color: "#fff",
+    textAlign: "center",
+    width: "70vw",
+    padding: 10,
+    marginTop: 10,
+    position: "relative",
+    top: -70,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "2.5rem",
+      top: 90,
+      width: "60vw",
+    },
+    [theme.breakpoints.up("md")]: {
+      fontSize: "2rem",
+      top: -30,
+      width: "35vw",
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "2.8rem",
+      top: 0,
+      width: "35vw",
+    },
+  },
+});
+
 const Login = (props) => {
   const history = useHistory();
-  const { user, register } = props;
+  const {
+    user,
+    register,
+    classes: { root, boxContainer, boxImage, boxText },
+  } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
 
   const handleRegister = async (event) => {
@@ -37,7 +111,19 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
+    <Grid className={root} container>
+      <Box
+        className={boxContainer}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <img className={boxImage} src={bubbleImg} alt="bubble" />
+        <Typography className={boxText}>
+          Converse with anyone with any language
+        </Typography>
+      </Box>
       <Box>
         <Grid container item>
           <Typography>Need to log in?</Typography>
@@ -121,4 +207,7 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(styles)(Login));
