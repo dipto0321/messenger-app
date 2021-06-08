f7b6ad32b56c215258ff7fe7d8f725eff3cb44ab
Dipto Karmakar <diptokmk47@gmail.com>
2021-06-08 21:36:27 +0600

    :bug: Message now showing after sending

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -74,6 +74,10 @@ export const logout = (id) => async (dispatch) => {
 export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get("/api/conversations");
+    console.log(
+      "🚀 ~ file: thunkCreators.js ~ line 75 ~ fetchConversations ~ data",
+      data
+    );
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -95,9 +99,9 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
+    const data = await saveMessage(body);
 
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
@@ -105,7 +109,7 @@ export const postMessage = (body) => (dispatch) => {
       dispatch(setNewMessage(data.message));
     }
 
-    sendMessage(data, body);
+    await sendMessage(data, body);
   } catch (error) {
     console.error(error);
   }
