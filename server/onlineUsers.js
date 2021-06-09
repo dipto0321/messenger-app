dcac8d8c7349d9aff02e35950e4db89906429919
Dipto Karmakar <diptokmk47@gmail.com>
2021-06-10 03:14:15 +0600

    :art: Change onlineUsers structure

--- server/onlineUsers.js
+++ server/onlineUsers.js
@@ -1,2 +1,19 @@
-const onlineUsers = [];
-module.exports = onlineUsers;
+let onlineUsers = [];
+const addOnlineUser = (userId, socketId) => {
+  !onlineUsers.some((user) => user.userId === userId) &&
+    onlineUsers.push({ userId, socketId });
+};
+
+const removeOnlineUser = (userId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== userId);
+};
+
+const getOnlineUser = (userId) => {
+  return onlineUsers.find((user) => user.userId === userId);
+};
+
+module.exports = {
+  addOnlineUser,
+  removeOnlineUser,
+  getOnlineUser,
+};
