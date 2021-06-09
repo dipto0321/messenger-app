f7a3ff54335bac8d4f8809a1cbc9f87b438c9465
Dipto Karmakar <diptokmk47@gmail.com>
2021-06-09 12:46:02 +0600

    :art: Login refactored

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
-import { connect } from "react-redux";
-import { withStyles } from "@material-ui/core/styles";
-
+import { useDispatch, useSelector } from "react-redux";
 import {
   Grid,
   Box,
@@ -11,171 +9,33 @@ import {
   FormControl,
   TextField,
 } from "@material-ui/core";
+import { LandingPageSidebar } from "./components";
+import { authUseStyles } from "./stylesHook";
 import { login } from "./store/utils/thunkCreators";
-
-import backgroundImg from "./assets/bg-img.png";
-import bubbleImg from "./assets/bubble.svg";
-
-const styles = (theme) => ({
-  root: {
-    height: "100vh",
-  },
-  leftBoxContainer: {
-    backgroundImage: `linear-gradient(rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)),url(${backgroundImg})`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "100vw 100vh",
-    width: "100vw",
-    height: "65vh",
-    [theme.breakpoints.up("sm")]: {
-      backgroundSize: "100vw",
-      height: "95vh",
-    },
-    [theme.breakpoints.up("md")]: {
-      backgroundSize: "50vw",
-      width: "50vw",
-      height: "100vh",
-    },
-    [theme.breakpoints.up("lg")]: {
-      backgroundSize: "40vw",
-      width: "40vw",
-    },
-  },
-  boxImage: {
-    width: "4rem",
-    position: "relative",
-    top: 64,
-    [theme.breakpoints.up("sm")]: {
-      top: 120,
-      width: "6.25rem",
-    },
-    [theme.breakpoints.up("md")]: {
-      top: -55,
-      width: "5rem",
-    },
-    [theme.breakpoints.up("lg")]: {
-      top: 10,
-      width: "7.5rem",
-    },
-  },
-  boxText: {
-    fontSize: "1.5rem",
-    color: "#fff",
-    textAlign: "center",
-    width: 300,
-    padding: 10,
-    marginTop: 10,
-    position: "relative",
-    top: 70,
-    [theme.breakpoints.up("sm")]: {
-      fontSize: "2.5rem",
-      top: 130,
-      width: 415,
-    },
-    [theme.breakpoints.up("md")]: {
-      fontSize: "2rem",
-      top: -40,
-      width: 340,
-    },
-    [theme.breakpoints.up("lg")]: {
-      fontSize: "2rem",
-      top: 0,
-      width: 340,
-    },
-  },
-  rightBoxContainer: {
-    width: "100vw",
-    padding: 20,
-    [theme.breakpoints.up("md")]: {
-      width: "40vw",
-      padding: "2.5rem",
-    },
-    [theme.breakpoints.up("lg")]: {
-      width: "54vw",
-      padding: "2rem",
-    },
-  },
-  button: {
-    color: "#3A8DFF",
-    backgroundColor: "#fff",
-    fontSize: 15,
-    "&:hover": {
-      color: "#fff",
-      backgroundColor: "#3A8DFF",
-    },
-    [theme.breakpoints.up("sm")]: {
-      fontSize: "1.25rem",
-    },
-  },
-  buttonSubmit: {
-    padding: "0.625rem 2.5rem",
-    [theme.breakpoints.up("sm")]: {
-      padding: "0.625rem 4.5rem",
-      fontSize: "1rem",
-    },
-    [theme.breakpoints.up("lg")]: {
-      padding: "0.625rem 7.5rem",
-      fontSize: "1.25rem",
-    },
-  },
-  text: {
-    color: "#B0B0B0",
-    fontSize: 14,
-    marginRight: 10,
-    [theme.breakpoints.up("sm")]: {
-      fontSize: "1.2rem",
-      marginRight: 20,
-    },
-  },
-  headLine: {
-    fontWeight: "600",
-    fontSize: 16,
-    textAlign: "start",
-    marginBottom: "1rem",
-    [theme.breakpoints.up("sm")]: {
-      fontSize: "1.6rem",
-    },
-  },
-  formContentContainer: {
-    margin: "1.5rem 0",
-    textAlign: "center",
-    [theme.breakpoints.up("sm")]: {
-      margin: "5rem 0",
-    },
-    [theme.breakpoints.up("lg")]: {
-      margin: "7rem 0",
-    },
-  },
-  formControlContainer: {
-    marginBottom: "1.5rem",
-  },
-});
 
 const Login = (props) => {
   const history = useHistory();
+
   const {
-    user,
-    login,
-    classes: {
-      root,
-      leftBoxContainer,
-      boxImage,
-      boxText,
-      rightBoxContainer,
-      button,
-      text,
-      formControlContainer,
-      buttonSubmit,
-      headLine,
-      formContentContainer,
-    },
-  } = props;
+    root,
+    rightBoxContainer,
+    button,
+    text,
+    formControlContainer,
+    buttonSubmit,
+    headLine,
+    formContentContainer,
+  } = authUseStyles();
+
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
 
   const handleLogin = async (event) => {
     event.preventDefault();
     const username = event.target.username.value;
     const password = event.target.password.value;
 
-    await login({ username, password });
+    await dispatch(login({ username, password }));
   };
 
   if (user.id) {
@@ -184,18 +44,7 @@ const Login = (props) => {
 
   return (
     <Grid className={root} container>
-      <Box
-        className={leftBoxContainer}
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <img className={boxImage} src={bubbleImg} alt="bubble" />
-        <Typography className={boxText}>
-          Converse with anyone with any language
-        </Typography>
-      </Box>
+      <LandingPageSidebar />
       <Box className={rightBoxContainer}>
         <Grid
           container
@@ -254,21 +103,4 @@ const Login = (props) => {
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    login: (credentials) => {
-      dispatch(login(credentials));
-    },
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(Login));
+export default Login;
---
+++ client/src/stylesHook/auth.js
@@ -0,0 +1,73 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  rightBoxContainer: {
+    width: "100vw",
+    padding: 20,
+    [theme.breakpoints.up("md")]: {
+      width: "40vw",
+      padding: "2.5rem",
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "54vw",
+      padding: "2rem",
+    },
+  },
+  button: {
+    color: "#3A8DFF",
+    backgroundColor: "#fff",
+    fontSize: 15,
+    "&:hover": {
+      color: "#fff",
+      backgroundColor: "#3A8DFF",
+    },
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.25rem",
+    },
+  },
+  buttonSubmit: {
+    padding: "0.625rem 2.5rem",
+    [theme.breakpoints.up("sm")]: {
+      padding: "0.625rem 4.5rem",
+      fontSize: "1rem",
+    },
+    [theme.breakpoints.up("lg")]: {
+      padding: "0.625rem 7.5rem",
+      fontSize: "1.25rem",
+    },
+  },
+  text: {
+    color: "#B0B0B0",
+    fontSize: 14,
+    marginRight: 10,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.2rem",
+      marginRight: 20,
+    },
+  },
+  headLine: {
+    fontWeight: "600",
+    fontSize: 16,
+    textAlign: "start",
+    marginBottom: "1rem",
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "1.6rem",
+    },
+  },
+  formContentContainer: {
+    margin: "1.5rem 0",
+    textAlign: "center",
+    [theme.breakpoints.up("sm")]: {
+      margin: "5rem 0",
+    },
+    [theme.breakpoints.up("lg")]: {
+      margin: "7rem 0",
+    },
+  },
+  formControlContainer: {
+    marginBottom: "1.5rem",
+  },
+}));
