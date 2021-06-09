039267854d41f695208b16f763f4e4355c4dd6d9
Dipto Karmakar <diptokmk47@gmail.com>
2021-06-09 14:19:36 +0600

    :lipstick: Update UI

--- client/src/stylesHook/auth.js
+++ client/src/stylesHook/auth.js
@@ -2,29 +2,41 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   rightBoxContainer: {
-    padding: 20,
+    padding: ".625rem",
+    margin: ".625rem",
     flexGrow: 1,
+    [theme.breakpoints.up("sm")]: {
+      padding: "1rem",
+      margin: "1rem",
+    },
+    [theme.breakpoints.up("lg")]: {
+      padding: "2rem",
+      margin: "1rem",
+    },
   },
   button: {
     color: "#3A8DFF",
     backgroundColor: "#fff",
     fontSize: 15,
+    boxShadow: "0px 2px 4px 1px #B0B0B0",
+    padding: "0.75rem 2rem",
     "&:hover": {
       color: "#fff",
       backgroundColor: "#3A8DFF",
     },
     [theme.breakpoints.up("sm")]: {
       fontSize: "1.25rem",
+      padding: "1.5rem 4.5rem",
     },
   },
   buttonSubmit: {
-    padding: "0.625rem 2.5rem",
+    padding: "0.75rem 2.5rem",
     [theme.breakpoints.up("sm")]: {
-      padding: "0.625rem 4.5rem",
+      padding: "1.5rem 4.5rem",
       fontSize: "1rem",
     },
     [theme.breakpoints.up("lg")]: {
-      padding: "0.625rem 7.5rem",
+      padding: "1.5rem 7.5rem",
       fontSize: "1.25rem",
     },
   },
@@ -50,10 +62,16 @@ export default makeStyles((theme) => ({
     margin: "1.5rem 0",
     textAlign: "center",
     [theme.breakpoints.up("sm")]: {
-      margin: "5rem 0",
+      margin: "4rem 0",
+      padding: "0 5rem",
+    },
+    [theme.breakpoints.up("md")]: {
+      margin: "2rem 0",
+      padding: "0 2rem",
     },
     [theme.breakpoints.up("lg")]: {
       margin: "7rem 0",
+      padding: "0 7rem",
     },
   },
   formControlContainer: {
