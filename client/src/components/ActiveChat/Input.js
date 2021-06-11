1876223c9fdbbef90845269960d4bf8f819204b3
Dipto Karmakar <diptokmk47@gmail.com>
2021-06-12 05:27:45 +0600

    :lipstick: Fix responsive UI

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -1,86 +1,95 @@
-import React, { Component } from "react";
-import { FormControl, FilledInput } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
-import { connect } from "react-redux";
+import { useState } from "react";
+import { Paper, InputBase, IconButton } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  SentimentSatisfiedOutlined,
+  FileCopyOutlined,
+} from "@material-ui/icons/";
+import { useDispatch, useSelector } from "react-redux";
 import { postMessage } from "../../store/utils/thunkCreators";
 
-const styles = {
+const useStyles = makeStyles((theme) => ({
   root: {
     justifySelf: "flex-end",
     marginTop: 15,
+    marginBottom: 20,
+    borderRadius: 8,
+    backgroundColor: "#F4F6FA",
+    height: 70,
+    boxShadow: "none",
   },
   input: {
     height: 70,
+    width: "70%",
     backgroundColor: "#F4F6FA",
-    borderRadius: 8,
-    marginBottom: 20,
+    padding: 10,
+    [theme.breakpoints.up("sm")]: {
+      width: "75%",
+    },
+    [theme.breakpoints.up("md")]: {
+      width: "80%",
+    },
+    [theme.breakpoints.up("lg")]: {
+      width: "90%",
+    },
   },
-};
+}));
 
-class Input extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
-    };
-  }
+const Input = ({ conversationId, otherUser }) => {
+  const [state, setState] = useState({
+    text: "",
+  });
 
-  handleChange = (event) => {
-    this.setState({
+  const dispatch = useDispatch();
+
+  const { root, input, iconButton } = useStyles();
+  const user = useSelector((state) => state.user);
+
+  const handleChange = (event) => {
+    setState({
       text: event.target.value,
     });
   };
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
     const reqBody = {
       text: event.target.text.value,
-      recipientId: this.props.otherUser.id,
-      conversationId: this.props.conversationId,
-      sender: this.props.conversationId ? null : this.props.user,
+      recipientId: otherUser.id,
+      conversationId: conversationId,
+      sender: conversationId ? null : user,
     };
-    await this.props.postMessage(reqBody);
-    this.setState({
+    await dispatch(postMessage(reqBody));
+    setState({
       text: "",
     });
   };
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <form className={classes.root} onSubmit={this.handleSubmit}>
-        <FormControl fullWidth hiddenLabel>
-          <FilledInput
-            classes={{ root: classes.input }}
-            disableUnderline
-            placeholder="Type something..."
-            value={this.state.text}
-            name="text"
-            onChange={this.handleChange}
-          />
-        </FormControl>
-      </form>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    user: state.user,
-    conversations: state.conversations,
-  };
+  return (
+    <Paper
+      component="form"
+      className={root}
+      onSubmit={handleSubmit}
+      rounded
+      outlined
+    >
+      <InputBase
+        classes={{ root: input }}
+        disableUnderline
+        placeholder="Type something..."
+        value={state.text}
+        name="text"
+        onChange={handleChange}
+      />
+      <IconButton className={iconButton}>
+        <SentimentSatisfiedOutlined />
+      </IconButton>
+      <IconButton className={iconButton}>
+        <FileCopyOutlined />
+      </IconButton>
+    </Paper>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    postMessage: (message) => {
-      dispatch(postMessage(message));
-    },
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withStyles(styles)(Input));
+export default Input;
